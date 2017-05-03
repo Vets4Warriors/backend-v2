7433eb0393e771ef433807028e6cf2d0cdd1d0d1
austin <austin.cawley@gmail.com>
2017-05-03 14:07:40 -0400

    Switch to yarn and incorporated the frontend
    Signed-off-by: austin <austin.cawley@gmail.com>

--- static/js/main.js
+++
@@ -1,3 +0,0 @@
-// This is where all the javascript for all pages should be put
-import '../scss/main.scss';
-
--- static/js/shared.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by austin on 4/30/17.
- */
-import './main';
-import '../scss/shared.scss';
